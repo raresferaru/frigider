943ae8bb80c277a3e49404b784529b4523de1fb5
raresferaru <64482832+raresferaru@users.noreply.github.com>
2023-03-28 13:23:15 +0300

    descriptive message

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+// const session = require('express-session')
+const path = require('path');
+const ejs = require('ejs')
+// const ExpressErorr = require('./utils/ExpressError');
+const formidable = require('formidable');
+const fileUpload = require('express-fileupload');
+const app = express();
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
+const Jimp = require('jimp');
+const sharp = require('sharp')
+const multiparty = require('multiparty');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(fileUpload());
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+async function imageOverlay(photos, size, top, left) { // Function name is same as of file
+    let x = 0;
+    let y = 0;
+
+    const image = await Jimp.read(__dirname + '/fridge.png');
+    for (let i = 1; i <= photos.length; i++) {
+        let watermark = await Jimp.read(__dirname + '/upload/' + `photo${i}.png`);
+        watermark = watermark.resize(400 * size / 100, 400 * size / 100); // Resizing watermark image
+        // Reading original image
+        x = 400 * left[i - 1] / 100;
+        y = 400 * top[i - 1] / 100;
+        watermark = await watermark
+        image.composite(watermark, x, y, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacityDest: 1,
+            opacitySource: 1
+        })
+
+        // images.push(image);
+    }
+    await image.writeAsync(__dirname + '/public' + '/jimp/' + 'newImage.png');
+
+}
+
+app.get('/home', (req, res) => {
+    res.render("home")
+})
+
+app.post('/', async (req, res) => {
+
+    for (let i = 1; i <= 6; i++) {
+        let filePath = __dirname + '/upload/' + `photo${i}.png`;
+        try { fs.unlinkSync(filePath); } catch { }
+        filePath = __dirname + '/nobg_upload/' + `photo${i}.png`;
+        try { fs.unlinkSync(filePath); } catch { };
+    }
+
+    const { p1, p2, p3, p4, p5, p6 } = req.files;
+    let photos = [];
+
+    photos.push(p1);
+    photos.push(p2);
+    photos.push(p3);
+    photos.push(p4);
+    photos.push(p5);
+    photos.push(p6);
+
+    let cnt = 0;
+    for (let p of photos) {
+        cnt += 1;
+        p.name = `photo${cnt}.png`;
+        await p.mv(__dirname + '/upload/' + p.name)
+
+    }
+
+    const { size, top, left } = req.body;
+    console.log(size);
+
+    imageOverlay(photos, size, top, left);
+    // res.redirect('/home')
+    res.render('send')
+})
+
+app.get('/cacat', (req, res) => {
+    res.render('send')
+})
+
+app.listen(5000, () => {
+    console.log("ready, master");
+})
+
---
+++ public/js/home.js
@@ -0,0 +1,55 @@
+const prevs = document.querySelectorAll('.prev');
+const input = document.querySelectorAll('input[type=number]')
+
+input[12].addEventListener('keyup', () => {
+
+    for (let prev of prevs) {
+        prev.height = input[12].value;
+        prev.width = input[12].value;
+    }
+})
+
+input[12].addEventListener('click', () => {
+
+    for (let prev of prevs) {
+        prev.height = input[12].value;
+        prev.width = input[12].value;
+    }
+})
+
+let cnt = 0;
+for (let prev of prevs) {
+    //console.log(prev)
+    cnt++;
+    if (cnt <= 3) {
+        input[cnt * 2 - 1].value = `${(cnt - 1) * 40 + 20}`;
+        input[cnt * 2 - 2].value = '20';
+        prev.style.top = '20px';
+        prev.style.left = `${(cnt - 1) * 40 + 20}px`
+    } else {
+        input[cnt * 2 - 1].value = `${(cnt - 4) * 40 + 20}`;
+        input[cnt * 2 - 2].value = '57';
+        prev.style.top = '57px';
+        prev.style.left = `${(cnt - 4) * 40 + 20}px`
+    }
+
+    prev.height = '30';
+    prev.width = '30';
+}
+
+
+for (let i = 0; i <= 10; i += 2) {
+    //console.log(prevs[i / 2])
+    input[i].addEventListener('keyup', () => {
+        prevs[i / 2].style.top = input[i].value + 'px';
+    })
+    input[i].addEventListener('click', () => {
+        prevs[i / 2].style.top = input[i].value + 'px';
+    })
+}
+
+for (let i = 1; i <= 11; i += 2) {
+    input[i].addEventListener('click', () => {
+        prevs[Math.floor(i / 2)].style.left = input[i].value + 'px';
+    })
+}
